e4df4c153be8e9428c4d99f28859877c1d4a9ddf
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-05 13:59:47 +0800

    [ST001TDD][Robert] test: Added @BeforeEach for FizzBuzz declaration, and added test case for returning Fizz given a number that is a multiple of 3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,18 +1,26 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+    @BeforeEach
+    public void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
 
     @Test
     void should_return_special_number_given_special_number_is_not_a_multiple_of_any_special_number() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String specialNumber = "2";
         assertThat(fizzBuzz.countOff(specialNumber)).isEqualTo("2");
+    }
+
+    @Test
+    void should_return_Fizz_given_special_number_is_a_multiple_of_three() {
+        String specialNumber = "3";
+        assertThat(fizzBuzz.countOff(specialNumber)).isEqualTo("Fizz");
     }
 }
