d389a97f410c6a5d239a34b3503c1a7d3a2bf427
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-05 14:29:30 +0800

    [ST001TDD][Robert] test: Added test case for testing different numbers and evaluate if output and concatenating is correct

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -3,6 +3,10 @@ package tdd.fizzbuzz;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FizzBuzzTest {
@@ -34,5 +38,18 @@ public class FizzBuzzTest {
     void should_return_Buzz_given_special_number_is_a_multiple_of_seven() {
         String specialNumber = "7";
         assertThat(fizzBuzz.countOff(specialNumber)).isEqualTo("Whizz");
+    }
+
+    @Test
+    void countOff_should_return_expected_output_given_combinations_of_numbers() {
+        List<String> numbersList = Arrays.asList("1","3","5","7","9","12","15","18","21","22","23","24","25","26","27","28","29","30");
+        List<String> expectedOutputList = Arrays.asList("1","Fizz","Buzz","Whizz","Fizz","Fizz","FizzBuzz",
+                "Fizz","FizzWhizz","22","23","Fizz","Buzz","26","Fizz","Whizz","29","FizzBuzz");
+        List<String> outputList = new ArrayList<>();
+        for(String number : numbersList) {
+            outputList.add(fizzBuzz.countOff(number));
+        }
+        assertThat(expectedOutputList.toString()).isEqualTo(outputList.toString());
+
     }
 }
