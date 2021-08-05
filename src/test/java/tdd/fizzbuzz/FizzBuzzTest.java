d45551cf16a5b7b0de0ad962b270314688af7eb2
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-05 13:49:54 +0800

    [ST001TDD][Robert] test: Added test case for returning number if not a multiple of special numbers

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_return_special_number_given_special_number_is_not_a_multiple_of_any_special_number() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String specialNumber = "2";
+        assertThat(fizzBuzz.countOff(specialNumber)).isEqualTo("2");
+    }
 }
